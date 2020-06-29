d7d6b217df49ede0d819b7f83a5987254935164e
thaibao0225 <57218923+thaibao0225@users.noreply.github.com>
2020-06-29 13:16:58 +0700

    da xong login

--- connect.js
+++ connect.js
@@ -7,19 +7,31 @@ const url = "mongodb+srv://AdminATN:123456ATN@cluster0-bqt2b.mongodb.net/ATN-Sho
 const NameDataBase = "ATN-Shop";
 const NameTable = "Account";
 
+var username = "admin";
+var password = "123456";
 
-/// --------------------Find-------------------------
+
+// /// --------------------Find-------------------------
 // MongoClient.connect(url, { useUnifiedTopology: true })
 // .then (client => {
 //   var dbo = client.db(NameDataBase);
 //   dbo.collection(NameTable).find({}).toArray()
 // 	.then (result => {
-// 		console.log(result);
+//         //console.log(result);
+//         console.log(result.length)
+//         console.log("--------------");
+//         for(var i= 0; i < result.length; i++)
+//         {
+//             var bodyy = result[i].User;
+//         console.log(bodyy);
+//         }
 // 		client.close();
 // 	})
 // 	.catch(error => console.error(error));
 // })
 // .catch(error => console.error(error));
+
+
 
 // /// --------------------Insert-------------------------
 // MongoClient.connect(url, { useUnifiedTopology: true })
@@ -48,4 +60,28 @@ const NameTable = "Account";
 
 
 
+/// --------------------Query-------------------------
+MongoClient.connect(url, { useUnifiedTopology: true })
+.then (client => {
+  var dbo = client.db(NameDataBase);
+    var query = {
+        User: "admin",
+        Password:"123456"
+};
 
+  dbo.collection(NameTable).find(query).toArray()
+	.then (result => {
+        //console.log(result);
+        console.log(result.length)
+        console.log("--------------");
+        for(var i= 0; i < result.length; i++)
+        {
+
+        console.log(result[i].User);
+        console.log(result[i].Password);
+        }
+		client.close();
+	})
+	.catch(error => console.error(error));
+})
+.catch(error => console.error(error));
--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(express.static("public"));
 var xflag = 0;
 var vResult = [];
 
-
 /// ***************** ***************** *****************
 /// ***************** ***************** Config DB CONNECTION
 const MongoClient = require('mongodb').MongoClient;
@@ -38,7 +37,8 @@ const NameDataBase = "ATN-Shop";
 const NameTable = "Account";
 
 
-
+/// ***************** ***************** *****************
+/// ***************** GET
 app.get("/", function(reg,res){
     res.render("home");
 });
@@ -61,40 +61,7 @@ app.get("/product", function(req,res)
 });
 
 
-app.post('/register', function (req, res) {
-    var body = req.body;
-    
-        /// --------------------Insert-------------------------
-    MongoClient.connect(uri, { useUnifiedTopology: true })
-    .then (client => {
-    var dbo = client.db(NameDataBase);
-    var mydata = {
-        User: "1",
-        Password: "2",
-        Permission: "3",
-        Fullname: "4",
-        DateOfBirth: "5",
-        Address: "6",
-        Sex: "7",
-        Phone: "8",
-        Email:"9",
-        CN_id: "10"
-    };
-    dbo.collection(NameTable).insertOne(body)
-        .then (result => {
-            console.log(result);
-            client.close();
-        })
-        .catch(error => console.error(error));
-    })
-    .catch(error => console.error(error));
 
-
-    console.log(body);
-
-    res.redirect('/');
-    
-  });
 
 app.get("/account", function(req,res)
 {
@@ -112,6 +79,73 @@ app.get("/register",function(req,res)
     res.render("register");
 });
 
+/// ***************** ***************** *****************
+/// ***************** POST
+app.post('/register', function (req, res) {
+    var body = req.body;
+    
+        /// --------------------Insert-------------------------
+    MongoClient.connect(uri, { useUnifiedTopology: true })
+    .then (client => {
+    var dbo = client.db(NameDataBase);
+    
+    dbo.collection(NameTable).insertOne(body)
+        .then (result => {
+            console.log(result);
+            client.close();
+        })
+        .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+
+
+    console.log(body);
+
+    res.redirect('/');
+    
+});
+
+app.post('/login', function(req, res){
+    var body = req.body;
+    //console.log(body.User);
+    //console.log(body.Password);
+
+    /// --------------------Query-------------------------
+    MongoClient.connect(uri, { useUnifiedTopology: true })
+    .then (client => {
+    var dbo = client.db(NameDataBase);
+        var query = {
+            User: body.User,
+            Password: body.Password
+    };
+
+    dbo.collection(NameTable).find(query).toArray()
+        .then (result => {
+            //console.log(result);
+            // console.log(result.length)
+            // console.log("--------------");
+            // for(var i= 0; i < result.length; i++)
+            // {
+
+            // console.log(result[i].User);
+            // console.log(result[i].Password);
+            // }
+
+            if(result.length == 1)
+            {
+                console.log("ban da dang nhap thanh cong");
+            }
+            client.close();
+        })
+        .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+
+    res.redirect('/');
+});
 
 app.set('views', path.join(__dirname, './views'));
 app.listen(port, () => console.log("Example app listening at http://localhost:${port}"));
+
+
+

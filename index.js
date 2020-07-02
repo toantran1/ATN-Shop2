abf6e0cf5c234226774cbed3c3b41e77737c6d00
thaibao0225 <57218923+thaibao0225@users.noreply.github.com>
2020-07-02 20:16:36 +0700

    tiep tuc payment

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(express.static("public"));
 
 var xflag = 0;
 var vResult = [];
+var blockPayment = 0;
 
 /// ***************** ***************** *****************
 /// ***************** ***************** Config DB CONNECTION
@@ -56,6 +57,8 @@ app.get("/login",function(req,res)
 
 app.get("/product", function(req,res)
 {
+    blockPayment = 0 ;
+    arrBill = [];
     responseDB(res, "product",
 				Product, {}, {}, "productlist");
     //res.render("product");
@@ -86,33 +89,52 @@ app.get("/register",function(req,res)
 /// ***************** ***************** *****************
 app.get('/payment', viewPayment);
 function viewPayment(request, response) {
-       //response.send("Web - PAYMENT page !" + request.query.dssp);
-       var dssp = request.query.dssp;
-       var listkq = dssp.split("_");
-   
-       listsp = [];
-       var count = listkq.length/2; 
+    //response.send("Web - PAYMENT page !" + request.query.dssp);
+    var dssp = request.query.dssp;
+    var listkq = dssp.split("_");
 
-       if (listkq.length % 2 != 0 )
-        count = ((listkq.length - 1) /2);
+    listsp = [];
+    var count = listkq.length/2; 
 
-       for (i=0; i< count; i++) {
-           listsp.push(
-               { Name :listkq[i*2], Price : 30000, Num: listkq[i*2+1]},
-           );
+    if (listkq.length % 2 != 0 )
+    count = ((listkq.length - 1) /2);
 
-       }
-    
+    for (i=0; i< count; i++) {
+        listsp.push(
+            { Name :listkq[i*2], Price : 30000, Num: listkq[i*2+1]},
+        );
+
+    }
+
+    if(blockPayment == 0)
+    {
         for (j=0; j< count; j++){
             query = {
                 _id: ObjectId(listsp[j].Name)
             };
-
+    
             runQuery("Products",query);
         }
-        
+        blockPayment = 1;
+    }
+    else
+    {
+        if (blockPayment == 1)
+        {
+            for(var i = 0; i< count; i++)
+            {
+                arrBill[0].Price *= listkq[i*2+1];
+                arrBill[0].Num = listkq[i*2+1];
+            }
+            blockPayment = 2;
+        }
+
+    }
     
-        response.render("payment", {productlist : arrBill });
+    
+
+
+    response.render("payment", {productlist : arrBill });
        
         
 
